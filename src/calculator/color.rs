c8da2d32d6ef6887ffcf6fc1a2663773f8d8b4d9
david072 <david.g.ganz@gmail.com>
2022-09-10 19:22:19 +0200

    fix(colors): Use the same color sequence for each bracket stack
    We now reset our random number generator everytime we get back to the
    top level with the brackets to generate the same sequence of colors.
    
    e.g.
    Same color Same color
    ((((())))) ((((()))))

--- src/calculator/color.rs
+++ src/calculator/color.rs
@@ -6,11 +6,12 @@
 
 use std::ops::Range;
 use eframe::egui::Color32;
-use rand::Rng;
+use rand::{Rng, SeedableRng, rngs::SmallRng};
 use crate::astgen::tokenizer::{Token, TokenType};
 use self::TokenType::*;
 
 const IDENTIFIER_COLOR: Color32 = Color32::LIGHT_BLUE;
+const INITIAL_RNG_SEED: u64 = 420;
 
 #[derive(Debug, Clone)]
 pub struct ColorSegment {
@@ -29,16 +30,29 @@ impl ColorSegment {
         let mut last_token: Option<(TokenType, Range<usize>)> = None;
         let mut bracket_colors = Vec::<Color32>::new();
 
+        let mut rng = SmallRng::seed_from_u64(INITIAL_RNG_SEED);
+        let mut nesting = 0usize;
+
         for token in tokens {
             match token.ty {
                 OpenBracket => {
-                    let color = random_color();
+                    let r = rng.gen::<u8>();
+                    let g = rng.gen::<u8>();
+                    let b = rng.gen::<u8>();
+                    let color = Color32::from_rgb(r, g, b);
+
                     result.push(ColorSegment::new(token.range.clone(), color));
                     bracket_colors.push(color);
+                    nesting += 1;
                 }
                 CloseBracket => {
                     let color = bracket_colors.pop().unwrap_or(Color32::WHITE);
                     result.push(ColorSegment::new(token.range.clone(), color));
+
+                    nesting -= 1;
+                    if nesting == 0 {
+                        rng = SmallRng::seed_from_u64(INITIAL_RNG_SEED);
+                    }
                 }
                 Divide if last_token.is_some() &&
                     last_token.as_ref().unwrap().0 == Identifier &&
@@ -70,12 +84,4 @@ impl ColorSegment {
             color,
         }
     }
-}
-
-fn random_color() -> Color32 {
-    let mut rng = rand::thread_rng();
-    let r = rng.gen::<u8>();
-    let g = rng.gen::<u8>();
-    let b = rng.gen::<u8>();
-    Color32::from_rgb(r, g, b)
 }
