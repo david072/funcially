7f4a234f39b02ab8647de5cd2805a7abc9dd02ca
david072 <david.g.ganz@gmail.com>
2022-12-05 20:07:45 +0100

    fix(engine): correctly transfer units when evaluating sub-expressions
    We now prefer the variable that was assigned to the sub-expression (e.g.
    variable, function, group), meaning the unit of the evaluated result
    will be overridden.

--- calculator/src/engine.rs
+++ calculator/src/engine.rs
@@ -399,7 +399,7 @@ impl<'a> Engine<'a> {
                 }
             };
             let mut new_node = AstNode::from(node, AstNodeData::Literal(result));
-            new_node.unit = unit;
+            if new_node.unit.is_none() { new_node.unit = unit; }
             let _ = replace(node, new_node);
         }
 
@@ -418,7 +418,7 @@ impl<'a> Engine<'a> {
                 Err(ty) => return Err(ty.with(node.range.clone())),
             };
             let mut new_node = AstNode::from(node, AstNodeData::Literal(*number));
-            new_node.unit = unit.clone();
+            if new_node.unit.is_none() { new_node.unit = unit.clone(); }
             let _ = replace(node, new_node);
         }
 
@@ -435,7 +435,7 @@ impl<'a> Engine<'a> {
             let group_result = Self::evaluate(group_ast.clone(), self.env, self.currencies)?;
             // Construct Literal node with the evaluated result
             let mut new_node = AstNode::from(node, AstNodeData::Literal(group_result.result));
-            new_node.unit = group_result.unit;
+            if new_node.unit.is_none() { new_node.unit = group_result.unit; }
             let _ = replace(node, new_node);
         }
 
