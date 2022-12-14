696022f29c54bac4f09592107b00cca305a032ea
david072 <david.g.ganz@gmail.com>
2022-12-14 17:07:59 +0100

    feat(tokenizer): allow _ in identifiers

--- calculator/src/astgen/tokenizer.rs
+++ calculator/src/astgen/tokenizer.rs
@@ -104,7 +104,7 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>> {
 }
 
 const NUMBERS: &str = "0123456789_";
-const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
 const HEXADECIMAL_CHARS: &str = "0123456789abcdefABCDEF_";
 const BINARY_DIGITS: &str = "01_";
 const WHITESPACE: &str = " \t\r\n";
