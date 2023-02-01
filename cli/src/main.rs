5fea338b0558e7b5c1b37958aef51a1eb79c9e57
david072 <david.g.ganz@gmail.com>
2023-02-01 17:47:01 +0100

    feat(cli): cli argument to calculate a single input
    Using the syntax `<cli> -- <input>`, the CLI will calculate <input>,
    print the result and return immediately.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -38,8 +38,12 @@ fn main() {
         .arg(Arg::new("thousands_separator")
             .short('s')
             .long("thousands_separator")
-            .help("Whether to use a '_' as the thousands separator in the results")
+            .help("Use a '_' as the thousands separator in the results")
             .action(ArgAction::SetTrue))
+        .arg(Arg::new("input")
+            .help("Calculate a single string and print the result")
+            .last(true)
+            .action(ArgAction::Append))
         .get_matches();
 
     let verbosity = match matches.get_one::<String>("verbosity") {
@@ -59,6 +63,12 @@ fn main() {
 
     let mut calculator = Calculator::new(verbosity, settings);
 
+    if let Some(input) = matches.get_many::<String>("input") {
+        let input = input.fold(String::new(), |acc, s| acc + s );
+        calculate_and_print(input, &mut calculator, use_thousands_separator);
+        return;
+    }
+
     // TODO: Properly handle CTRL-C
     loop {
         print!("> ");
@@ -68,100 +78,108 @@ fn main() {
         stdin().read_line(&mut input).unwrap();
         input = input.trim().to_string();
 
-        let mut words = input.split(' ').filter(|s| !s.is_empty());
-        let first_word = words.next().map(|s| s.to_lowercase());
-        if matches!(input.as_str(), "quit" | "exit") {
+        if !calculate_and_print(input, &mut calculator, use_thousands_separator) {
             break;
         }
+    }
+}
 
-        if first_word.is_some() && first_word.clone().unwrap() == "set" {
-            let Some(path) = words.next() else {
-                eprintln!("{}", "Expected a setting.".red());
-                continue;
-            };
-            let path = path.split('.').collect::<Vec<_>>();
+fn calculate_and_print(input: String, calculator: &mut Calculator, use_thousands_separator: bool) -> bool {
+    let mut words = input.split(' ').filter(|s| !s.is_empty());
+    let first_word = words.next().map(|s| s.to_lowercase());
+    if matches!(input.as_str(), "quit" | "exit") {
+        return false;
+    }
 
-            let next = words.next();
-            if next.is_some() && next.unwrap() == "?" {
-                let mut path = path;
-                path.push("");
-                let err = calculator.settings.set(&path, "").unwrap_err();
-                if let AccessError::InvalidPath(options) = err { println!("Options: {options:?}") }
-                continue;
-            }
+    if first_word.is_some() && first_word.clone().unwrap() == "set" {
+        let Some(path) = words.next() else {
+            eprintln!("{}", "Expected a setting.".red());
+            return true;
+        };
+        let path = path.split('.').collect::<Vec<_>>();
 
-            if next.is_none() || next.unwrap() != "=" {
-                eprintln!("{}", "Expected equals sign.".red());
-                eprintln!("{}", "Hint: Put a question mark at the end of the line to see the available options.".cyan());
-                continue;
-            }
-            let Some(value) = words.next() else {
-                eprintln!("{}", "Expected the value.".red());
-                continue;
-            };
-            if words.next().is_some() {
-                eprintln!("{}", "Too many arguments.".red());
-                continue;
-            }
-
-            if let Err(e) = calculator.settings.set(&path, value) {
-                eprintln!("{}", e.to_string().red());
-            } else if let Ok(contents) = ron::to_string(&calculator.settings) {
-                let _ = std::fs::write(settings_path(), contents);
-            }
-            continue;
-        } else if first_word.is_some() && first_word.unwrap() == "get" {
-            let Some(path) = words.next() else {
-                eprintln!("{}", "Expected a setting.".red());
-                continue;
-            };
-
-            if words.next().is_some() {
-                eprintln!("{}", "Too many arguments.".red());
-                continue;
-            }
-
-            let path = path.split('.').collect::<Vec<_>>();
-            match calculator.settings.get(&path) {
-                Ok(v) => println!("Setting's value: {v:?}"),
-                Err(e) => eprintln!("{}", e.to_string().red()),
-            }
-            continue;
+        let next = words.next();
+        if next.is_some() && next.unwrap() == "?" {
+            let mut path = path;
+            path.push("");
+            let err = calculator.settings.set(&path, "").unwrap_err();
+            if let AccessError::InvalidPath(options) = err { println!("Options: {options:?}") }
+            return true;
         }
 
-        match input.as_str() {
-            "quit" | "exit" => break,
-            _ => {
-                match calculator.calculate(&input) {
-                    Ok(res) => match res.data {
-                        ResultData::Value(value) => {
-                            println!("= {}", value.format(&calculator.settings, use_thousands_separator));
-                        }
-                        ResultData::Boolean(b) => {
-                            println!("=> {}", if b { "True".green() } else { "False".red() });
-                        }
-                        ResultData::Function { .. } | ResultData::Nothing => {}
-                    },
-                    Err(error) => {
-                        eprintln!("{}: {}", "Error".red(), error.error);
+        if next.is_none() || next.unwrap() != "=" {
+            eprintln!("{}", "Expected equals sign.".red());
+            eprintln!("{}", "Hint: Put a question mark at the end of the line to see the available options.".cyan());
+            return true;
+        }
+        let Some(value) = words.next() else {
+            eprintln!("{}", "Expected the value.".red());
+            return true;
+        };
+        if words.next().is_some() {
+            eprintln!("{}", "Too many arguments.".red());
+            return true;
+        }
 
-                        let slice_start = std::cmp::max(0, error.start as isize - 5) as usize;
-                        let slice_end = std::cmp::min(input.len(), error.end + 5);
-                        let slice = &input[slice_start..slice_end];
-                        eprintln!("{slice}");
+        if let Err(e) = calculator.settings.set(&path, value) {
+            eprintln!("{}", e.to_string().red());
+        } else if let Ok(contents) = ron::to_string(&calculator.settings) {
+            let _ = std::fs::write(settings_path(), contents);
+        }
+        return true;
+    } else if first_word.is_some() && first_word.unwrap() == "get" {
+        let Some(path) = words.next() else {
+            eprintln!("{}", "Expected a setting.".red());
+            return true;
+        };
 
-                        for _ in 0..error.start - slice_start {
-                            eprint!(" ");
-                        }
-                        eprint!("{}", "^".cyan());
-                        for _ in 0..error.end - error.start - 1 {
-                            eprint!("{}", "-".cyan());
-                        }
+        if words.next().is_some() {
+            eprintln!("{}", "Too many arguments.".red());
+            return true;
+        }
 
-                        eprintln!(" {}", error.error.to_string().cyan());
+        let path = path.split('.').collect::<Vec<_>>();
+        match calculator.settings.get(&path) {
+            Ok(v) => println!("Setting's value: {v:?}"),
+            Err(e) => eprintln!("{}", e.to_string().red()),
+        }
+        return true;
+    }
+
+    match input.as_str() {
+        "quit" | "exit" => return false,
+        _ => {
+            match calculator.calculate(&input) {
+                Ok(res) => match res.data {
+                    ResultData::Value(value) => {
+                        println!("= {}", value.format(&calculator.settings, use_thousands_separator));
                     }
+                    ResultData::Boolean(b) => {
+                        println!("=> {}", if b { "True".green() } else { "False".red() });
+                    }
+                    ResultData::Function { .. } | ResultData::Nothing => {}
+                },
+                Err(error) => {
+                    eprintln!("{}: {}", "Error".red(), error.error);
+
+                    let slice_start = std::cmp::max(0, error.start as isize - 5) as usize;
+                    let slice_end = std::cmp::min(input.len(), error.end + 5);
+                    let slice = &input[slice_start..slice_end];
+                    eprintln!("{slice}");
+
+                    for _ in 0..error.start - slice_start {
+                        eprint!(" ");
+                    }
+                    eprint!("{}", "^".cyan());
+                    for _ in 0..error.end - error.start - 1 {
+                        eprint!("{}", "-".cyan());
+                    }
+
+                    eprintln!(" {}", error.error.to_string().cyan());
                 }
             }
         }
     }
+
+    true
 }
