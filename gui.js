a8761085838542a6e718c2d3e5297a12d7fef206
david072 <david.g.ganz@gmail.com>
2023-02-16 11:17:50 +0000

    Deployment from david072/funcially@d27b367a3fb86cf4a529e7edb7abf17ab6754e4a

--- gui.js
+++ gui.js
@@ -1400,16 +1400,16 @@ function getImports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3736 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1212, __wbg_adapter_28);
+    imports.wbg.__wbindgen_closure_wrapper3744 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1213, __wbg_adapter_28);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3737 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1212, __wbg_adapter_31);
+    imports.wbg.__wbindgen_closure_wrapper3745 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1213, __wbg_adapter_31);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3817 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1244, __wbg_adapter_34);
+    imports.wbg.__wbindgen_closure_wrapper3825 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1245, __wbg_adapter_34);
         return addHeapObject(ret);
     };
 
